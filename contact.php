82bb874ac2b58d02015c8b6749e829e76e0665bf
Aaronpanderson <44983947+Aaronpanderson@users.noreply.github.com>
2018-11-13 11:12:22 -0600

    Update contact.php

--- contact.php
+++ contact.php
@@ -2,23 +2,17 @@
   $name = $_POST['name'];
   $visitor_email = $_POST['email'];
   $message = $_POST['message'];
-?>
 
-<?php
     $email_from = 'contact@aaronpanderson.com';
     $email_subject = "New Form submission";
     $email_body = "You have received a new message from the user $name.\n".
                             "Here is the message:\n $message".
-?>
 
-<?php
   $to = "contact@aaronpanderson.com";
   $headers = "From: $email_from \r\n";
   $headers .= "Reply-To: $visitor_email \r\n";
   mail($to,$email_subject,$email_body,$headers);
- ?>
 
-<?php
 function IsInjected($str)
 {
     $injections = array('(\n+)',
